a3d04a43dc83cd9aaa266a3a62109e0d553e81f5
inesgdr <gadri.ines@yahoo.fr>
2017-12-13 15:47:37 +0100

    ps

--- routes/web.php
+++ routes/web.php
@@ -15,20 +15,7 @@ Route::get('/', function () {
     return view('forms.index');
 });
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> f24ff59374c856162baae83232dcd45aba2b71ec
 
-
-
-<<<<<<< HEAD
 Route::resource('/input','FormController'); // Route vers page after submit
-=======
->>>>>>> 0c0cbb93b83ca383bb7b1050e4e02c61e1e33caf
-=======
-
 
 // Route::resource('/input','FormController'); // Route vers page after submit
-
->>>>>>> f24ff59374c856162baae83232dcd45aba2b71ec
