1fd06be33660f9916bf95acac600faea736044b1
inesgdr <gadri.ines@yahoo.fr>
2017-12-14 14:25:12 +0100

    'test'

---
+++ resources/views/mails/contact.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="utf-8">
+</head>
+<body>
+<h2>Mail</h2>
+<p>Réception d'une prise de contact avec les éléments suivants :</p>
+<ul>
+<li><strong>Nom</strong> : {{ $contact['nom'] }}</li>
+<li><strong>Email</strong> : {{ $contact['youremail'] }}</li>
+<li><strong>Emailto</strong> : {{ $contact['email'] }}</li>
+</ul>
+</body>
+</html>
