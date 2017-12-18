c0d73f28262964e42e1dac8378aeade1fe3c1eef
tim007221 <tim.guerre@sfr.fr>
2017-12-18 15:19:35 +0100

    df

--- resources/views/mails/contact.blade.php
+++ resources/views/mails/contact.blade.php
@@ -9,12 +9,12 @@
 <body>
 
 
-<h2>Vous avez recu un fichier</h2>
-
-<p>Le fichier a été envoyer de <p>
+<h2>Vous avez recu un fichier de {{$username}} </h2>
 
 
-<p>Pour télecharger ce fichier veuillez cliquer sur ce magnifique <a href='http://79.137.83.180:8002/uploads/{{$nomfichier}} ' download="1" >lien</a></p>
+
+
+<p>Pour télecharger ce fichier veuillez cliquer sur ce <a href='http://79.137.83.180:8002/uploads/{{$nomfichier}} ' download="1" >lien</a></p>
 
 
 
