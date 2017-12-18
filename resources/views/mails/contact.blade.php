db522a9845b3d008de58728239550bad7cad1c22
tim007221 <tim.guerre@sfr.fr>
2017-12-18 13:58:57 +0100

    df

--- resources/views/mails/contact.blade.php
+++ resources/views/mails/contact.blade.php
@@ -14,7 +14,7 @@
 <p>Le fichier a été envoyer de <p>
 
 
-<p>Pour télecharger ce fichier veuillez cliquer sur ce magnifique <a href='79.137.83.180:8002/uploads/{{$nomfichier}}'>lien</a></p>
+<p>Pour télecharger ce fichier veuillez cliquer sur ce magnifique <a href='http://79.137.83.180:8002/uploads/{{$nomfichier}}'>lien</a></p>
 
 
 
