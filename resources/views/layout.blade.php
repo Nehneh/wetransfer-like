3163b265cccc3ac9eae6beccba8ffeb6f78b388c
tim007221 <tim.guerre@sfr.fr>
2017-12-19 11:35:20 +0100

    modif layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,14 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-	<meta charset="utf-8">
-	<title> @yield ('title')</title>
-		<title></title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" >
-	{{ Html::style('css/style.css') }}
-
-</head>
-
+@include('template.header')
 <body>
 
 	@yield ('content')
---
+++ resources/views/template/header.blade.php
@@ -0,0 +1,7 @@
+<head>
+	<meta charset="utf-8">
+	<title> @yield ('title')</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" >
+	{{ Html::style('css/style.css') }}
+
+</head>
