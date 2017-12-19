b824e9789de3b14dec64db7d0180b3bf53376fc7
inesgdr <gadri.ines@yahoo.fr>
2017-12-19 16:22:35 +0100

    finitions

--- resources/views/forms/store.blade.php
+++ resources/views/forms/store.blade.php
@@ -6,10 +6,13 @@
 
 @section ('content')
 			
-	
+	<div class="container">
+		<div class="row">
 			<div  class="col-sm-4"></div>
-			<div  class="col-sm-4 center"><h1 class= "bleu">Le fichier a bien été envoyé</h1></div>
+			<div  class="col-sm-4 center"><h1>Le fichier a bien été envoyé, votre destinaire recevra un mail afin d'accéder à ce dernier. Merci et à bientôt</h1></div>
 			<div  class="col-sm-4"></div>
+		</div>
+	</div>
 @endsection
 
 
